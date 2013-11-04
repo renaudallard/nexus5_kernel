cfafd4b67081913f832c2a36a49b7e150ff22e19
Renaud Allard <renaud@allard.it>
2013-11-04 14:19:09 +0100

    Add defconfig, initramfs, and building scripts

---
+++ release/repack_bootimg.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+RAMV=$1
+KVER=`cat ../.version`
+RELV="`cat ../.version`-${RAMV}"
+RELV="_FuguMod_$(date +%Y%m%d_r)${RELV}"
+if [ "ZZ${RAMV}" == "ZZ" ]
+then
+	echo "No version specified"
+	exit 1
+else
+	cd initramfs-${RAMV} || exit 1
+	find . | cpio -o -H newc | lzop > ../new_initramfs.cpio.lzo
+	cd ..
+	abootimg --create ${HOME}/kernel${RELV}.img -k ../arch/arm/boot/zImage -r new_initramfs.cpio.lzo
+
+	git tag -a r${KVER} -m r${KVER}
+
+	cd cwm
+	mv ${HOME}/kernel${RELV}.img boot.img
+        zip -q -r ${HOME}/kernel${RELV}.zip boot.img META-INF || exit 1
+        sha256sum ${HOME}/kernel${RELV}.zip > ${HOME}/kernel${RELV}.zip.sha256sum
+
+	scp ${HOME}/kernel* isildur.arnor.org:public_html/nexus5/testing-${RAMV}/
+
+	cd ..
+	rm new_initramfs.cpio.lzo cwm/boot.img ${HOME}/kernel*
+	
+fi
+
---
+++ setenv.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+export VENDOR=fugu
+export ARCH=arm 
+export TOOLCHAIN=~/x-tools/arm-${VENDOR}-linux-gnueabi
+export TOOLPREFIX=arm-${VENDOR}-linux-gnueabi
+#export TOOLCHAIN=~/android-ndk-r8d/toolchains/arm-linux-androideabi-4.6/prebuilt/linux-x86
+#export TOOLPREFIX=arm-linux-androideabi
+export PATH=$PATH:${TOOLCHAIN}/bin:/usr/java/jdk1.6.0_21/bin
+export CROSS_COMPILE=${TOOLCHAIN}/bin/${TOOLPREFIX}-
+export CC=${TOOLCHAIN}/bin/${TOOLPREFIX}-gcc
+export LD=${TOOLCHAIN}/bin/${TOOLPREFIX}-ld
+export AR=${TOOLCHAIN}/bin/${TOOLPREFIX}-ar
+export RANLIB=${TOOLCHAIN}/arm-${VENDOR}-linux-gnueabi/bin/ranlib
+export PATH=$PATH:${TOOLCHAIN}/bin
+export PATH=$PATH:${TOOLCHAIN}/
+export PATH=$PATH:${TOOLCHAIN}/lib
+#export CFLAGS="-mcpu=cortex-a8 -mfpu=neon -mfloat-abi=softfp -static -fstack-protector -fstack-protector-all -fno-gcse -fprefetch-loop-arrays --param l2-cache-size=512 --param l1-cache-size=64 --param simultaneous-prefetches=6 --param prefetch-latency=400 --param l1-cache-line-size=64"
+export CFLAGS="-static -fstack-protector -fstack-protector-all -mfpu=neon -mcpu=cortex-a9"
+export LDFLAGS="-static -z relro -z now"
+export CPPFLAGS="-static"
+export KBUILD_BUILD_HOST=fugu
+export KBUILD_BUILD_USER=r
+
+alias gcp='git cherry-pick -x '
+alias rel='make -j20 && cd release && ./repack_bootimg.sh 4.4 && cd ..'
+
+echo "ARM environment set"
